2b1e14c681fae1b876d1bade84d96dfb30e3e7ab
Barry ODriscoll <barryodriscoll@gmail.com>
2011-03-24 16:08:54 +0000

    [BoD] Added push task to rakefile

--- push.sh
+++ push.sh
@@ -4,11 +4,17 @@ set -e
 
 OUTPUT="" 
 
+echo "cleaning ec2 node..."
+
 ssh -i ~/ec2/Neba-Laptop.pem ubuntu@ec2-46-51-161-60.eu-west-1.compute.amazonaws.com "rm -rf ~/development/elog/*" 2>&1 $OUTPUT
 echo $OUTPUT
+
+echo "copying files to ec2 node..."
 
 scp -r -p -i /Users/barryodriscoll/ec2/Neba-Laptop.pem /Users/barryodriscoll/Documents/crc_workspace/elog/* ubuntu@ec2-46-51-161-60.eu-west-1.compute.amazonaws.com:/home/ubuntu/development/elog 2>&1 $OUTPUT
 echo $OUTPUT
 
-ssh -i ~/ec2/Neba-Laptop.pem ubuntu@ec2-46-51-161-60.eu-west-1.compute.amazonaws.com "source /home/ubuntu/.profile; cd /home/ubuntu/development/elog; pwd; bjam --clean release toolset=gcc; bjam release toolset=gcc; ./bin/gcc/release/elog > email.txt" 2>&1 $OUTPUT
+echo "building and running on ec2 node..."
+
+ssh -i ~/ec2/Neba-Laptop.pem ubuntu@ec2-46-51-161-60.eu-west-1.compute.amazonaws.com "cd /home/ubuntu/development/elog; pwd; rake build; rake run" 2>&1 $OUTPUT
 echo $OUTPUT
